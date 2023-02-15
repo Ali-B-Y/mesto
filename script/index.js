11d7d7df62e903f1bcc8904ec907e132dc2f97c5
Alisher <berdievalisher@inbox.ru>
2023-02-15 12:34:04 +0300

    отклонение от итерации, отформатировал css файлы

--- script/index.js
+++ script/index.js
@@ -1,16 +1,16 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-nickname');
-let jobInput = document.querySelector('.popup__input-job');
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input-nickname');
+const jobInput = document.querySelector('.popup__input-job');
 const profileNickname = document.querySelector('.profile__nickname');
 const profileJob = document.querySelector('.profile__job')
 
 const handleEditButtonClick = () => {
 	nameInput.value = profileNickname.textContent;
 	jobInput.value = profileJob.textContent;
-    popup.classList.add('popup_opened');
+	popup.classList.add('popup_opened');
 }
 
 const handleCloseButtonClick = () => {
@@ -18,15 +18,15 @@ const handleCloseButtonClick = () => {
 }
 
 const handlePopupClick = (event) => {
-	if(event.target === event.currentTarget) {
+	if (event.target === event.currentTarget) {
 		popup.classList.remove('popup_opened');
 	}
 }
 
-function handleFormSubmit (event) {
-    event.preventDefault();
+function handleFormSubmit(event) {
+	event.preventDefault();
 	profileNickname.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
+	profileJob.textContent = jobInput.value;
 	popup.classList.remove('popup_opened');
 }
 
