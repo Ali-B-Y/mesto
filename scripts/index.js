831de358179dff610f693c917a3fa46779bbc64a
Alisher <berdievalisher@inbox.ru>
2023-03-06 15:49:02 +0300

    проверка связи

--- scripts/index.js
+++ scripts/index.js
@@ -10,14 +10,19 @@ const buttonCloseAddCard = popupAddCard.querySelector('.popup__close-button');
 
 // попап просмотр увеличенного фото
 const popupViewPhoto = document.querySelector('.popup_js_view-photo');
-const buttonCloseViewPhoto = popupViewPhoto.querySelector('.popup-view__close-button');
+const buttonCloseViewPhoto = popupViewPhoto.querySelector('.popup__close-button');
 
 // форма профиля
-const form = document.querySelector('.popup__form');
+const formEditProfile = document.querySelector('form[name="edit-profile"]');
 const nicknameInput = document.querySelector('.popup__input_js_nickname');
 const descInput = document.querySelector('.popup__input_js_desc');
 const profileNickname = document.querySelector('.profile__nickname');
 const profileDesc = document.querySelector('.profile__desc');
+
+// форма добавления карточки
+const formAddCard = document.querySelector('form[name="add-card"]');
+const photoNameInput = document.querySelector('.popup__input_js_photo-name');
+const photoUrlInput = document.querySelector('.popup__input_js_photo-url');
 
 // контейнер для будущих карточек и шаблон карточек
 const cardsContainer = document.querySelector('.cards-container');
@@ -71,8 +76,8 @@ const createCard = ((data) => {
     card.remove();
   }
   const handleCardPhoto = () => {
-    const popupPhoto = popupViewPhoto.querySelector('.popup-view__photo');
-    const popupTitle = popupViewPhoto.querySelector('.popup-view__title');
+    const popupPhoto = popupViewPhoto.querySelector('.popup__photo');
+    const popupTitle = popupViewPhoto.querySelector('.popup__photo-caption');
     popupPhoto.src = cardPhoto.src;
     popupPhoto.alt = cardPhoto.alt;
     popupTitle.textContent = cardTitle.textContent;
@@ -97,16 +102,7 @@ initialCards.forEach(el => {
 });
 
 
-// const handlePopupClick = (event) => {
-// 	if (event.target === event.currentTarget) {
-// 		popup.classList.remove('popup_opened');
-// 	}
-// }
-
-//popup.addEventListener('click', handlePopupClick);
-
-
-
+// слушатель и обработчик кнопки редкатироваеия профиля
 const handleEditProfile = () => {
   nicknameInput.value = profileNickname.textContent;
   descInput.value = profileDesc.textContent;
@@ -118,6 +114,7 @@ buttonCloseEditProfile.addEventListener('click', () => {
   closePopup(popupEditProfile);
 });
 
+// слушатель и обработчик кнопки добавить карточку
 const handleAddCard = () => {
   openPopup(popupAddCard);
 }
@@ -127,15 +124,74 @@ buttonCloseAddCard.addEventListener('click', () => {
   closePopup(popupAddCard);
 });
 
-function handleFormSubmit(event) {
-  event.preventDefault();
+// слушатель и обработчик формы профиля
+const handleFormEditProfileSubmit = evt => {
+  evt.preventDefault();
   profileNickname.textContent = nicknameInput.value;
   profileDesc.textContent = descInput.value;
   closePopup(popupEditProfile);
 }
+formEditProfile.addEventListener('submit', handleFormEditProfileSubmit);
 
-form.addEventListener('submit', handleFormSubmit);
+// слушатель и обработчик формы добавления карточки пользователя
+const handleFormAddCardSubmit = evt => {
+  evt.preventDefault();
+  const userCard = {};
+  userCard.name = photoNameInput.value;
+  userCard.link = photoUrlInput.value;
+  console.log(userCard)
+  initialCards.unshift(userCard);
+  initialCards.pop();
+  initialCards.forEach(el => {
+    renderCard(el);
+  });
+  closePopup(popupAddCard);
+}
+
+  // const addUsersCard = (photoName, photoUrl) => {
+  //   const userCard = {};
+  //   userCard.name = photoName;
+  //   userCard.link = photoUrl;
+  //   initialCards.unshift(userCard);
+  //   initialCards.pop();
+  //   return userCard
+  // }
+
+  // ((photoName, photoUrl) => {
+  //   const userCard = {};
+  //   userCard.name = photoName;
+  //   userCard.link = photoUrl;
+  //   initialCards.unshift('userCard');
+  //   initialCards.pop();
+  //   console.log(userCard)
+  // })();
+
+
+// const addUsersCard = (photoName, photoUrl) => {
+//   const userCard = {};
+//   userCard.name = photoName;
+//   userCard.link = photoUrl;
+//   initialCards.unshift('userCard');
+//   initialCards.pop();
+// }
+//
+
+
+formAddCard.addEventListener('submit', handleFormAddCardSubmit);
+
+
+
 
 buttonCloseViewPhoto.addEventListener('click', () => {
   closePopup(popupViewPhoto);
 });
+
+
+
+// const handlePopupClick = (event) => {
+// 	if (event.target === event.currentTarget) {
+// 		popup.classList.remove('popup_opened');
+// 	}
+// }
+
+//popup.addEventListener('click', handlePopupClick);
